65cd21dafb8145724299d6caace0753979e68853
ImplementLife <lmplementlifeg@gmail.com>
2022-10-14 21:22:22 +0300

    Extends Billing model (add status)

--- src/main/java/com/implementLife/BankMock/entity/Billing.java
+++ src/main/java/com/implementLife/BankMock/entity/Billing.java
@@ -1,11 +1,16 @@
 package com.implementLife.BankMock.entity;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Billing {
     private UUID id;
 
     private BankAccount bankAccountReceiver;
+
+    private Date lastDateTimeForPay;
+
+    private char status;
 
     private String sum;
 }
